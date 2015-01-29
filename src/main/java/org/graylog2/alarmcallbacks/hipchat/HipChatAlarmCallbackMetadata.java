c09be68132ca5f07b2eea110daabaf0465bd81fb
Jochen Schalanda <jochen@torch.sh>
2015-01-29 15:40:23 +0100

    Migrate to Graylog 1.0.0-rc.1

--- src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackMetadata.java
+++ src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackMetadata.java
@@ -21,9 +21,12 @@
 package org.graylog2.alarmcallbacks.hipchat;
 
 import org.graylog2.plugin.PluginMetaData;
+import org.graylog2.plugin.ServerStatus;
 import org.graylog2.plugin.Version;
 
 import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
 
 public class HipChatAlarmCallbackMetadata implements PluginMetaData {
     @Override
@@ -43,12 +46,12 @@ public class HipChatAlarmCallbackMetadata implements PluginMetaData {
 
     @Override
     public URI getURL() {
-        return URI.create("http://www.graylog2.org/");
+        return URI.create("https://www.graylog2.org/");
     }
 
     @Override
     public Version getVersion() {
-        return new Version(1, 0, 0);
+        return new Version(1, 1, 0);
     }
 
     @Override
@@ -58,6 +61,11 @@ public class HipChatAlarmCallbackMetadata implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(0, 90, 0);
+        return new Version(1, 0, 0);
+    }
+
+    @Override
+    public Set<ServerStatus.Capability> getRequiredCapabilities() {
+        return Collections.emptySet();
     }
 }
--- src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackModule.java
+++ src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackModule.java
@@ -25,7 +25,6 @@ import org.graylog2.plugin.PluginModule;
 public class HipChatAlarmCallbackModule extends PluginModule {
     @Override
     protected void configure() {
-        registerPlugin(HipChatAlarmCallbackMetadata.class);
         addAlarmCallback(HipChatAlarmCallback.class);
     }
 }
