4c592a2b94cf524e209f803ded193c874a31a854
Edmundo Alvarez <edmundo@graylog.com>
2016-04-27 15:47:50 +0200

    Use graylog 2.0.0

--- src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackMetadata.java
+++ src/main/java/org/graylog2/alarmcallbacks/hipchat/HipChatAlarmCallbackMetadata.java
@@ -60,7 +60,7 @@ public class HipChatAlarmCallbackMetadata implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(1, 0, 0);
+        return new Version(2, 0, 0);
     }
 
     @Override
